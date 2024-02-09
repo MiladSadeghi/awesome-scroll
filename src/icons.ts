549194c79f2d251a254d3d5bfa1a33c9a28a3bbe
milad sadeghi <miladsadeghi2323@gmail.com>
2024-02-09 14:20:41 +0330

    feat: Added dock bar   - Created an icons object to store icon data.   - Added icons to relevant parts of the document.   - Implemented a dock bar with icon shortcuts.

---
+++ src/icons.ts
@@ -0,0 +1,73 @@
+export interface IIcon {
+  name: string;
+  path: string;
+}
+
+const icons: IIcon[] = [
+  {
+    name: "Facebook",
+    path: "/icons/facebook.svg",
+  },
+  {
+    name: "Figma",
+    path: "/icons/Figma.svg",
+  },
+  {
+    name: "Github",
+    path: "/icons/Github.svg",
+  },
+  {
+    name: "Spotify",
+    path: "/icons/Spotify.svg",
+  },
+  {
+    name: "Tiktok",
+    path: "/icons/Tiktok.svg",
+  },
+  {
+    name: "X",
+    path: "/icons/X.svg",
+  },
+  {
+    name: "Whatsapp",
+    path: "/icons/Whatsapp.svg",
+  },
+  {
+    name: "ُSnapshot",
+    path: "/icons/Snapshot.svg",
+  },
+  {
+    name: "Reddit",
+    path: "/icons/Reddit.svg",
+  },
+  {
+    name: "Notion",
+    path: "/icons/Notion.svg",
+  },
+  {
+    name: "Linkedin",
+    path: "/icons/Linkedin.svg",
+  },
+  {
+    name: "Vimo",
+    path: "/icons/Vimo.svg",
+  },
+  {
+    name: "Skype",
+    path: "/icons/Skype.svg",
+  },
+  {
+    name: "Apple",
+    path: "/icons/Apple.svg",
+  },
+  {
+    name: "Discord",
+    path: "/icons/Discord.svg",
+  },
+  {
+    name: "Dribbble",
+    path: "/icons/Dribbble.svg",
+  },
+];
+
+export default icons;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
+import icons, { IIcon } from "./icons";
 import "./style.css";
+
+const dockEl = document.querySelector(".dock");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fragment = document.createDocumentFragment();
+  for (let icon of icons) {
+    fragment.appendChild(createIcon(icon));
+  }
+  dockEl?.appendChild(fragment);
+});
+
+function createIcon(icon: IIcon) {
+  const iconEl = document.createElement("a");
+  iconEl.innerHTML = `
+    <img src="${icon.path}" alt="${icon.name}" />
+  `;
+  iconEl.classList.add("icon");
+  return iconEl;
+}
